3e10a28c244034290eff206fac883c57bfda9665
Mariana <mariana.latynina@gmail.com>
2020-10-20 09:29:03 +0100

    Update Martha's graphics manually

--- character_map_martha.py
+++ character_map_martha.py
@@ -25,76 +25,76 @@ characterMapMartha = {
   "martha_base_under_003":                 "martha_base_under_003",                 # Auto: Edited
   "martha_ex_001_001":                     "martha_ex_001_001",                     # Auto: Edited
   "martha_ex_001_smile_001":               "martha_ex_001_smile_001",               # Auto: Edited
-  "martha_ex_002_001":                     "martha_ex_002_001",                     # Auto: Edited
-  "martha_ex_002_smile_001":               "martha_ex_002_smile_001",               # Auto: Edited
-  "martha_ex_003_001":                     "martha_ex_003_001",                     # Auto: Edited
-  "martha_ex_004_001":                     "martha_ex_004_001",                     # Auto: Edited
-  "martha_ex_005_001":                     "martha_ex_005_001",                     # Auto: Edited
-  "martha_ex_005_closed_001":              "",                                      # Auto: Deleted
-  "martha_ex_005_grin2_001":               "",                                      # Auto: Deleted
-  "martha_ex_005_grin_001":                "",                                      # Auto: Deleted
-  "martha_ex_005_open_001":                "martha_ex_005_open_001",                # Auto: Edited
-  "martha_ex_006_001":                     "martha_ex_006_001",                     # Auto: Edited
-  "martha_ex_007_001":                     "",                                      # Auto: Deleted
-  "martha_ex_007_closed_001":              "",                                      # Auto: Deleted
-  "martha_ex_007_smile_001":               "",                                      # Auto: Deleted
-  "martha_ex_008_001":                     "",                                      # Auto: Deleted
-  "martha_ex_009_001":                     "",                                      # Auto: Deleted
-  "martha_ex_010_002":                     "martha_ex_010_002",                     # Auto: Edited
-  "martha_ex_011_002":                     "martha_ex_011_002",                     # Auto: Edited
-  "martha_ex_012_002":                     "martha_ex_012_002",                     # Auto: Edited
-  "martha_ex_013_002":                     "martha_ex_013_002",                     # Auto: Edited
-  "martha_ex_014_002":                     "martha_ex_014_002",                     # Auto: Edited
-  "martha_ex_015_002":                     "martha_ex_015_002",                     # Auto: Edited
-  "martha_ex_016_002":                     "martha_ex_016_002",                     # Auto: Edited
-  "martha_ex_017_002":                     "martha_ex_017_002",                     # Auto: Edited
-  "martha_ex_018_002":                     "martha_ex_018_002",                     # Auto: Edited
-  "martha_ex_019_002":                     "",                                      # Auto: Deleted
-  "martha_ex_020_002":                     "",                                      # Auto: Deleted
-  "martha_ex_021_002":                     "",                                      # Auto: Deleted
-  "martha_ex_022_002":                     "",                                      # Auto: Deleted
-  "martha_ex_023_003":                     "martha_ex_023_003",                     # Auto: Edited
-  "martha_ex_024_003":                     "martha_ex_024_003",                     # Auto: Edited
-  "martha_ex_025_003":                     "martha_ex_025_003",                     # Auto: Edited
-  "martha_ex_026_003":                     "",                                      # Auto: Deleted
-  "martha_ex_027_003":                     "",                                      # Auto: Deleted
-  "martha_ex_028_003":                     "",                                      # Auto: Deleted
-  "martha_ex_029_003":                     "",                                      # Auto: Deleted
-  "martha_ex_full_001_001":                "martha_ex_full_001_001",                # Auto: Edited
-  "martha_ex_full_001_smile_001":          "martha_ex_full_001_smile_001",          # Auto: Edited
-  "martha_ex_full_002_001":                "martha_ex_full_002_001",                # Auto: Edited
-  "martha_ex_full_002_smile_001":          "martha_ex_full_002_smile_001",          # Auto: Edited
-  "martha_ex_full_003_001":                "martha_ex_full_003_001",                # Auto: Edited
-  "martha_ex_full_004_001":                "martha_ex_full_004_001",                # Auto: Edited
-  "martha_ex_full_005_001":                "martha_ex_full_005_001",                # Auto: Edited
-  "martha_ex_full_005_closed_001":         "",                                      # Auto: Deleted
-  "martha_ex_full_005_grin2_001":          "",                                      # Auto: Deleted
-  "martha_ex_full_005_grin_001":           "",                                      # Auto: Deleted
-  "martha_ex_full_005_open_001":           "martha_ex_full_005_open_001",           # Auto: Edited
-  "martha_ex_full_006_001":                "martha_ex_full_006_001",                # Auto: Edited
-  "martha_ex_full_007_001":                "",                                      # Auto: Deleted
-  "martha_ex_full_007_closed_001":         "",                                      # Auto: Deleted
-  "martha_ex_full_007_smile_001":          "",                                      # Auto: Deleted
-  "martha_ex_full_008_001":                "",                                      # Auto: Deleted
-  "martha_ex_full_009_001":                "",                                      # Auto: Deleted
-  "martha_ex_full_010_002":                "martha_ex_full_010_002",                # Auto: Edited
-  "martha_ex_full_011_002":                "martha_ex_full_011_002",                # Auto: Edited
-  "martha_ex_full_012_002":                "martha_ex_full_012_002",                # Auto: Edited
-  "martha_ex_full_013_002":                "martha_ex_full_013_002",                # Auto: Edited
-  "martha_ex_full_014_002":                "martha_ex_full_014_002",                # Auto: Edited
-  "martha_ex_full_015_002":                "martha_ex_full_015_002",                # Auto: Edited
-  "martha_ex_full_016_002":                "martha_ex_full_016_002",                # Auto: Edited
-  "martha_ex_full_017_002":                "martha_ex_full_017_002",                # Auto: Edited
-  "martha_ex_full_018_002":                "martha_ex_full_018_002",                # Auto: Edited
-  "martha_ex_full_019_002":                "",                                      # Auto: Deleted
-  "martha_ex_full_020_002":                "",                                      # Auto: Deleted
-  "martha_ex_full_021_002":                "",                                      # Auto: Deleted
-  "martha_ex_full_022_002":                "",                                      # Auto: Deleted
-  "martha_ex_full_023_003":                "martha_ex_full_023_003",                # Auto: Edited
-  "martha_ex_full_024_003":                "martha_ex_full_024_003",                # Auto: Edited
-  "martha_ex_full_025_003":                "martha_ex_full_025_003",                # Auto: Edited
-  "martha_ex_full_026_003":                "",                                      # Auto: Deleted
-  "martha_ex_full_027_003":                "",                                      # Auto: Deleted
-  "martha_ex_full_028_003":                "",                                      # Auto: Deleted
-  "martha_ex_full_029_003":                "",                                      # Auto: Deleted
+  "martha_ex_002_001":                     "martha_ex_002_smile_001",
+  "martha_ex_002_smile_001":               "martha_ex_002_001",
+  "martha_ex_003_001":                     "martha_ex_005_001",
+  "martha_ex_004_001":                     "martha_ex_005_open_001",
+  "martha_ex_005_001":                     "martha_ex_003_open2_001",
+  "martha_ex_005_closed_001":              "martha_ex_003_irked_001",
+  "martha_ex_005_grin2_001":               "martha_ex_003_grin2_001",
+  "martha_ex_005_grin_001":                "martha_ex_003_grin_001",
+  "martha_ex_005_open_001":                "martha_ex_003_open_001",
+  "martha_ex_006_001":                     "martha_ex_005_aroused2_001",
+  "martha_ex_007_001":                     "martha_ex_004_open_001",
+  "martha_ex_007_closed_001":              "martha_ex_004_001",
+  "martha_ex_007_smile_001":               "martha_ex_004_smile_001",
+  "martha_ex_008_001":                     "martha_ex_004_aroused_001",
+  "martha_ex_009_001":                     "martha_ex_005_aroused3_001",
+  "martha_ex_010_002":                     "martha_ex_007_open_002",
+  "martha_ex_011_002":                     "martha_ex_008_open_002",
+  "martha_ex_012_002":                     "martha_ex_010_002",
+  "martha_ex_013_002":                     "martha_ex_009_closed_002",
+  "martha_ex_014_002":                     "martha_ex_009_002",
+  "martha_ex_015_002":                     "martha_ex_011_002",
+  "martha_ex_016_002":                     "martha_ex_012_002",
+  "martha_ex_017_002":                     "martha_ex_013_002",
+  "martha_ex_018_002":                     "martha_ex_014_002",
+  "martha_ex_019_002":                     "martha_ex_015_002",
+  "martha_ex_020_002":                     "martha_ex_016_002",
+  "martha_ex_021_002":                     "martha_ex_017_002",
+  "martha_ex_022_002":                     "martha_ex_018_002",
+  "martha_ex_023_003":                     "martha_ex_020_003",
+  "martha_ex_024_003":                     "martha_ex_019_003",
+  "martha_ex_025_003":                     "martha_ex_022_003",
+  "martha_ex_026_003":                     "martha_ex_025_003",
+  "martha_ex_027_003":                     "martha_ex_021_003",
+  "martha_ex_028_003":                     "martha_ex_024_003",
+  "martha_ex_029_003":                     "martha_ex_023_003",
+  "martha_ex_full_001_001":                "martha_ex_full_001_001",
+  "martha_ex_full_001_smile_001":          "martha_ex_full_001_smile_001",
+  "martha_ex_full_002_001":                "martha_ex_full_002_smile_001",
+  "martha_ex_full_002_smile_001":          "martha_ex_full_002_001",
+  "martha_ex_full_003_001":                "martha_ex_full_005_001",
+  "martha_ex_full_004_001":                "martha_ex_full_005_open_001",
+  "martha_ex_full_005_001":                "martha_ex_full_003_open2_001",
+  "martha_ex_full_005_closed_001":         "martha_ex_full_003_irked_001",
+  "martha_ex_full_005_grin2_001":          "martha_ex_full_003_grin2_001",
+  "martha_ex_full_005_grin_001":           "martha_ex_full_003_grin_001",
+  "martha_ex_full_005_open_001":           "martha_ex_full_003_open_001",
+  "martha_ex_full_006_001":                "martha_ex_full_005_aroused2_001",
+  "martha_ex_full_007_001":                "martha_ex_full_004_open_001",
+  "martha_ex_full_007_closed_001":         "martha_ex_full_004_001",
+  "martha_ex_full_007_smile_001":          "martha_ex_full_004_smile_001",
+  "martha_ex_full_008_001":                "martha_ex_full_004_aroused_001",
+  "martha_ex_full_009_001":                "martha_ex_full_005_aroused3_001",
+  "martha_ex_full_010_002":                "martha_ex_full_007_open_002",
+  "martha_ex_full_011_002":                "martha_ex_full_008_open_002",
+  "martha_ex_full_012_002":                "martha_ex_full_010_002",
+  "martha_ex_full_013_002":                "martha_ex_full_009_closed_002",
+  "martha_ex_full_014_002":                "martha_ex_full_009_002",
+  "martha_ex_full_015_002":                "martha_ex_full_011_002",
+  "martha_ex_full_016_002":                "martha_ex_full_012_002",
+  "martha_ex_full_017_002":                "martha_ex_full_013_002",
+  "martha_ex_full_018_002":                "martha_ex_full_014_002",
+  "martha_ex_full_019_002":                "martha_ex_full_015_002",
+  "martha_ex_full_020_002":                "martha_ex_full_016_002",
+  "martha_ex_full_021_002":                "martha_ex_full_017_002",
+  "martha_ex_full_022_002":                "martha_ex_full_018_002",
+  "martha_ex_full_023_003":                "martha_ex_full_020_003",
+  "martha_ex_full_024_003":                "martha_ex_full_019_003",
+  "martha_ex_full_025_003":                "martha_ex_full_022_003",
+  "martha_ex_full_026_003":                "martha_ex_full_025_003",
+  "martha_ex_full_027_003":                "martha_ex_full_021_003",
+  "martha_ex_full_028_003":                "martha_ex_full_024_003",
+  "martha_ex_full_029_003":                "martha_ex_full_023_003",
 }
