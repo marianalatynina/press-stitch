f5e89378fbaa27a6829546137df835aa1b94529d
Mariana <mariana.latynina@gmail.com>
2020-10-20 09:53:00 +0100

    Update Hillary graphics manually

--- character_map_hillary.py
+++ character_map_hillary.py
@@ -9,21 +9,21 @@ characterMapHillary = {
   "hillary_ex_001_001":                    "hillary_ex_001_001",                    # Auto: Same
   "hillary_ex_001_neutral_001":            "hillary_ex_001_neutral_001",            # Auto: Same
   "hillary_ex_001_smile_001":              "hillary_ex_001_smile_001",              # Auto: Same
-  "hillary_ex_002_001":                    "hillary_ex_002_smile_001",              # Auto: Renamed
-  "hillary_ex_002_smile_001":              "hillary_ex_002_001",                    # Auto: Renamed
+  "hillary_ex_002_001":                    "hillary_ex_002_001",
+  "hillary_ex_002_smile_001":              "hillary_ex_002_smile_001",
   "hillary_ex_003_001":                    "hillary_ex_003_001",                    # Auto: Same
   "hillary_ex_004_001":                    "hillary_ex_003_open2_001",              # Auto: Renamed
   "hillary_ex_004_grin2_001":              "hillary_ex_003_grin2_001",              # Auto: Renamed
   "hillary_ex_004_grin_001":               "hillary_ex_003_grin_001",               # Auto: Renamed
-  "hillary_ex_004_open_001":               "hillary_ex_004_open_001",               # Auto: Edited
+  "hillary_ex_004_open_001":               "hillary_ex_003_open_001",               # Not an exact match
   "hillary_ex_005_001":                    "hillary_ex_009_001",                    # Auto: Renamed
-  "hillary_ex_005_closed_001":             "",                                      # Auto: Deleted
+  "hillary_ex_005_closed_001":             "hillary_ex_002_open_001",
   "hillary_ex_006_001":                    "hillary_ex_004_open_001",               # Auto: Renamed
-  "hillary_ex_007_001":                    "hillary_ex_007_001",                    # Auto: Edited
+  "hillary_ex_007_001":                    "hillary_ex_004_open2_001",              # Not an exact match
   "hillary_ex_007_smile_001":              "hillary_ex_004_smile_001",              # Auto: Renamed
   "hillary_ex_008_001":                    "hillary_ex_008_open_001",               # Auto: Renamed
   "hillary_ex_009_001":                    "hillary_ex_007_001",                    # Auto: Renamed
-  "hillary_ex_010_001":                    "hillary_ex_010_001",                    # Auto: Edited
-  "hillary_ex_010_closed_001":             "",                                      # Auto: Deleted
-  "hillary_ex_011_001":                    "hillary_ex_011_001",                    # Auto: Edited
+  "hillary_ex_010_001":                    "hillary_ex_005_aroused_001",
+  "hillary_ex_010_closed_001":             "hillary_ex_006_open2_001",
+  "hillary_ex_011_001":                    "hillary_ex_012_001",
 }
