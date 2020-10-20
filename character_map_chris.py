6b59ba587a5a393d2a6292542f76d4c0bba5a4ec
Mariana <mariana.latynina@gmail.com>
2020-10-20 02:11:03 +0100

    Update Christine graphics manually

--- character_map_chris.py
+++ character_map_chris.py
@@ -23,88 +23,88 @@ characterMapChris = {
   "chris_base_under_001":                  "chris_base_under_001",                  # Auto: Edited
   "chris_base_under_002":                  "chris_base_under_002",                  # Auto: Edited
   "chris_base_under_003":                  "chris_base_under_003",                  # Auto: Edited
-  "chris_ex_001_001":                      "chris_ex_001_001",                      # Auto: Edited
-  "chris_ex_001_smile_001":                "chris_ex_001_smile_001",                # Auto: Edited
-  "chris_ex_002_001":                      "chris_ex_002_001",                      # Auto: Edited
-  "chris_ex_002_smile_001":                "chris_ex_002_smile_001",                # Auto: Edited
-  "chris_ex_003_001":                      "chris_ex_003_001",                      # Auto: Edited
-  "chris_ex_004_001":                      "chris_ex_004_001",                      # Auto: Edited
-  "chris_ex_005_001":                      "chris_ex_005_001",                      # Auto: Edited
-  "chris_ex_005_smile_001":                "chris_ex_005_smile_001",                # Auto: Edited
-  "chris_ex_006_001":                      "chris_ex_006_001",                      # Auto: Edited
-  "chris_ex_007_001":                      "chris_ex_007_001",                      # Auto: Edited
-  "chris_ex_007_smile_001":                "chris_ex_007_smile_001",                # Auto: Edited
-  "chris_ex_008_002":                      "",                                      # Auto: Deleted
-  "chris_ex_009_002":                      "chris_ex_009_002",                      # Auto: Edited
-  "chris_ex_010_002":                      "chris_ex_010_002",                      # Auto: Edited
-  "chris_ex_011_002":                      "chris_ex_011_002",                      # Auto: Edited
-  "chris_ex_011_grin2_002":                "",                                      # Auto: Deleted
-  "chris_ex_011_grin_002":                 "",                                      # Auto: Deleted
-  "chris_ex_011_open_002":                 "",                                      # Auto: Deleted
-  "chris_ex_012_002":                      "chris_ex_012_002",                      # Auto: Edited
-  "chris_ex_013_002":                      "chris_ex_013_002",                      # Auto: Edited
-  "chris_ex_014_002":                      "chris_ex_014_002",                      # Auto: Edited
-  "chris_ex_014_open_002":                 "",                                      # Auto: Deleted
-  "chris_ex_014_smile2_002":               "",                                      # Auto: Deleted
-  "chris_ex_014_smile_002":                "",                                      # Auto: Deleted
-  "chris_ex_015_002":                      "chris_ex_015_002",                      # Auto: Edited
-  "chris_ex_016_002":                      "chris_ex_016_002",                      # Auto: Edited
-  "chris_ex_016_closed_002":               "",                                      # Auto: Deleted
-  "chris_ex_017_002":                      "chris_ex_017_002",                      # Auto: Edited
-  "chris_ex_018_002":                      "chris_ex_018_002",                      # Auto: Edited
-  "chris_ex_019_003":                      "",                                      # Auto: Deleted
-  "chris_ex_020_003":                      "chris_ex_020_003",                      # Auto: Edited
-  "chris_ex_021_003":                      "chris_ex_021_003",                      # Auto: Edited
-  "chris_ex_022_003":                      "chris_ex_022_003",                      # Auto: Edited
-  "chris_ex_023_003":                      "chris_ex_023_003",                      # Auto: Edited
-  "chris_ex_024_003":                      "chris_ex_024_003",                      # Auto: Edited
-  "chris_ex_025_003":                      "chris_ex_025_003",                      # Auto: Edited
-  "chris_ex_026_003":                      "chris_ex_026_003",                      # Auto: Edited
-  "chris_ex_027_003":                      "",                                      # Auto: Deleted
-  "chris_ex_028_003":                      "",                                      # Auto: Deleted
-  "chris_ex_029_003":                      "",                                      # Auto: Deleted
-  "chris_ex_030_003":                      "",                                      # Auto: Deleted
-  "chris_ex_full_001_001":                 "chris_ex_full_001_001",                 # Auto: Edited
-  "chris_ex_full_001_smile_001":           "chris_ex_full_001_smile_001",           # Auto: Edited
-  "chris_ex_full_002_001":                 "chris_ex_full_002_001",                 # Auto: Edited
-  "chris_ex_full_002_smile_001":           "chris_ex_full_002_smile_001",           # Auto: Edited
-  "chris_ex_full_003_001":                 "chris_ex_full_003_001",                 # Auto: Edited
-  "chris_ex_full_004_001":                 "chris_ex_full_004_001",                 # Auto: Edited
-  "chris_ex_full_005_001":                 "chris_ex_full_005_001",                 # Auto: Edited
-  "chris_ex_full_005_smile_001":           "chris_ex_full_005_smile_001",           # Auto: Edited
-  "chris_ex_full_006_001":                 "chris_ex_full_006_001",                 # Auto: Edited
-  "chris_ex_full_007_001":                 "chris_ex_full_007_001",                 # Auto: Edited
-  "chris_ex_full_007_smile_001":           "chris_ex_full_007_smile_001",           # Auto: Edited
-  "chris_ex_full_008_002":                 "",                                      # Auto: Deleted
-  "chris_ex_full_009_002":                 "chris_ex_full_009_002",                 # Auto: Edited
-  "chris_ex_full_010_002":                 "chris_ex_full_010_002",                 # Auto: Edited
-  "chris_ex_full_011_002":                 "chris_ex_full_011_002",                 # Auto: Edited
-  "chris_ex_full_011_grin2_002":           "",                                      # Auto: Deleted
-  "chris_ex_full_011_grin_002":            "",                                      # Auto: Deleted
-  "chris_ex_full_011_open_002":            "",                                      # Auto: Deleted
-  "chris_ex_full_012_002":                 "chris_ex_full_012_002",                 # Auto: Edited
-  "chris_ex_full_013_002":                 "chris_ex_full_013_002",                 # Auto: Edited
-  "chris_ex_full_014_002":                 "chris_ex_full_014_002",                 # Auto: Edited
-  "chris_ex_full_014_open_002":            "",                                      # Auto: Deleted
-  "chris_ex_full_014_smile2_002":          "",                                      # Auto: Deleted
-  "chris_ex_full_014_smile_002":           "",                                      # Auto: Deleted
-  "chris_ex_full_015_002":                 "chris_ex_full_015_002",                 # Auto: Edited
-  "chris_ex_full_016_002":                 "chris_ex_full_016_002",                 # Auto: Edited
-  "chris_ex_full_016_closed_002":          "",                                      # Auto: Deleted
-  "chris_ex_full_017_002":                 "chris_ex_full_017_002",                 # Auto: Edited
-  "chris_ex_full_018_002":                 "chris_ex_full_018_002",                 # Auto: Edited
-  "chris_ex_full_019_003":                 "",                                      # Auto: Deleted
-  "chris_ex_full_020_003":                 "chris_ex_full_020_003",                 # Auto: Edited
-  "chris_ex_full_021_003":                 "chris_ex_full_021_003",                 # Auto: Edited
-  "chris_ex_full_022_003":                 "chris_ex_full_022_003",                 # Auto: Edited
-  "chris_ex_full_023_003":                 "chris_ex_full_023_003",                 # Auto: Edited
-  "chris_ex_full_024_003":                 "chris_ex_full_024_003",                 # Auto: Edited
-  "chris_ex_full_025_003":                 "chris_ex_full_025_003",                 # Auto: Edited
-  "chris_ex_full_026_003":                 "chris_ex_full_026_003",                 # Auto: Edited
-  "chris_ex_full_027_003":                 "",                                      # Auto: Deleted
-  "chris_ex_full_028_003":                 "",                                      # Auto: Deleted
-  "chris_ex_full_029_003":                 "",                                      # Auto: Deleted
-  "chris_ex_full_030_003":                 "",                                      # Auto: Deleted
+  "chris_ex_001_001":                      "chris_ex_021_003",
+  "chris_ex_001_smile_001":                "chris_ex_021_smile_003",
+  "chris_ex_002_001":                      "chris_ex_022_smile_003",
+  "chris_ex_002_smile_001":                "chris_ex_022_003",
+  "chris_ex_003_001":                      "chris_ex_023_irked_003",
+  "chris_ex_004_001":                      "chris_ex_025_smile_003",
+  "chris_ex_005_001":                      "chris_ex_024_open_003",
+  "chris_ex_005_smile_001":                "chris_ex_024_smile_003",
+  "chris_ex_006_001":                      "chris_ex_026_smile2_003",               # Closest I could find, not great match
+  "chris_ex_007_001":                      "chris_ex_023_grin_003",
+  "chris_ex_007_smile_001":                "chris_ex_023_grin2_003",
+  "chris_ex_008_002":                      "chris_ex_001_neutral_001",
+  "chris_ex_009_002":                      "chris_ex_003_grin3_001",
+  "chris_ex_010_002":                      "chris_ex_004_smile2_001",
+  "chris_ex_011_002":                      "chris_ex_003_001",
+  "chris_ex_011_grin2_002":                "chris_ex_003_grin2_001",
+  "chris_ex_011_grin_002":                 "chris_ex_003_grin_001",
+  "chris_ex_011_open_002":                 "chris_ex_003_open_001",
+  "chris_ex_012_002":                      "chris_ex_003_open2_001",
+  "chris_ex_013_002":                      "chris_ex_003_grit_001",
+  "chris_ex_014_002":                      "chris_ex_005_001",
+  "chris_ex_014_open_002":                 "chris_ex_005_open_001",
+  "chris_ex_014_smile2_002":               "chris_ex_005_smile_001",                # Mouth not right but rest of face is
+  "chris_ex_014_smile_002":                "chris_ex_005_smile_001",
+  "chris_ex_015_002":                      "chris_ex_004_brow_001",
+  "chris_ex_016_002":                      "chris_ex_004_open2_001",
+  "chris_ex_016_closed_002":               "chris_ex_004_001",
+  "chris_ex_017_002":                      "chris_ex_003_grit2_001",
+  "chris_ex_018_002":                      "chris_ex_008_open_001",
+  "chris_ex_019_003":                      "chris_ex_013_002",
+  "chris_ex_020_003":                      "chris_ex_014_002",
+  "chris_ex_021_003":                      "chris_ex_019_002",
+  "chris_ex_022_003":                      "chris_ex_010_002",
+  "chris_ex_023_003":                      "chris_ex_011_002",
+  "chris_ex_024_003":                      "chris_ex_009_closed_002",
+  "chris_ex_025_003":                      "chris_ex_009_002",
+  "chris_ex_026_003":                      "chris_ex_015_002",
+  "chris_ex_027_003":                      "chris_ex_005_aroused_002",
+  "chris_ex_028_003":                      "chris_ex_012_002",
+  "chris_ex_029_003":                      "chris_ex_016_002",
+  "chris_ex_030_003":                      "chris_ex_017_002",
+  "chris_ex_full_001_001":                 "chris_ex_full_021_003",
+  "chris_ex_full_001_smile_001":           "chris_ex_full_021_smile_003",
+  "chris_ex_full_002_001":                 "chris_ex_full_022_smile_003",
+  "chris_ex_full_002_smile_001":           "chris_ex_full_022_003",
+  "chris_ex_full_003_001":                 "chris_ex_full_023_irked_003",
+  "chris_ex_full_004_001":                 "chris_ex_full_025_smile_003",
+  "chris_ex_full_005_001":                 "chris_ex_full_024_open_003",
+  "chris_ex_full_005_smile_001":           "chris_ex_full_024_smile_003",
+  "chris_ex_full_006_001":                 "chris_ex_full_026_smile2_003",
+  "chris_ex_full_007_001":                 "chris_ex_full_023_grin_003",
+  "chris_ex_full_007_smile_001":           "chris_ex_full_023_grin2_003",
+  "chris_ex_full_008_002":                 "chris_ex_full_001_neutral_001",
+  "chris_ex_full_009_002":                 "chris_ex_full_003_grin3_001",
+  "chris_ex_full_010_002":                 "chris_ex_full_004_smile2_001",
+  "chris_ex_full_011_002":                 "chris_ex_full_003_001",
+  "chris_ex_full_011_grin2_002":           "chris_ex_full_003_grin2_001",
+  "chris_ex_full_011_grin_002":            "chris_ex_full_003_grin_001",
+  "chris_ex_full_011_open_002":            "chris_ex_full_003_open_001",
+  "chris_ex_full_012_002":                 "chris_ex_full_003_open2_001",
+  "chris_ex_full_013_002":                 "chris_ex_full_003_grit_001",
+  "chris_ex_full_014_002":                 "chris_ex_full_005_001",
+  "chris_ex_full_014_open_002":            "chris_ex_full_005_open_001",
+  "chris_ex_full_014_smile2_002":          "chris_ex_full_005_smile_001",
+  "chris_ex_full_014_smile_002":           "chris_ex_full_005_smile_001",
+  "chris_ex_full_015_002":                 "chris_ex_full_004_brow_001",
+  "chris_ex_full_016_002":                 "chris_ex_full_004_open2_001",
+  "chris_ex_full_016_closed_002":          "chris_ex_full_004_001",
+  "chris_ex_full_017_002":                 "chris_ex_full_003_grit2_001",
+  "chris_ex_full_018_002":                 "chris_ex_full_008_open_001",
+  "chris_ex_full_019_003":                 "chris_ex_full_013_002",
+  "chris_ex_full_020_003":                 "chris_ex_full_014_002",
+  "chris_ex_full_021_003":                 "chris_ex_full_019_002",
+  "chris_ex_full_022_003":                 "chris_ex_full_010_002",
+  "chris_ex_full_023_003":                 "chris_ex_full_011_002",
+  "chris_ex_full_024_003":                 "chris_ex_full_009_closed_002",
+  "chris_ex_full_025_003":                 "chris_ex_full_009_002",
+  "chris_ex_full_026_003":                 "chris_ex_full_015_002",
+  "chris_ex_full_027_003":                 "chris_ex_full_005_aroused_002",
+  "chris_ex_full_028_003":                 "chris_ex_full_012_002",
+  "chris_ex_full_029_003":                 "chris_ex_full_016_002",
+  "chris_ex_full_030_003":                 "chris_ex_full_017_002",
   "chris_mut_full_hair_001":               "chris_mut_full_hair_001",               # Auto: Edited
   "chris_mut_full_hair_002":               "chris_mut_full_hair_002",               # Auto: Edited
   "chris_mut_full_hair_003":               "chris_mut_full_hair_003",               # Auto: Edited
