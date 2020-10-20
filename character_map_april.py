5905694c18635c651a8e0bde4f5d00c2a2069c7d
Mariana <mariana.latynina@gmail.com>
2020-10-20 10:30:11 +0100

    Update April graphics manually

--- character_map_april.py
+++ character_map_april.py
@@ -8,24 +8,24 @@ characterMapApril = {
   "april_base_night_002":                  "april_base_night_002",                  # Auto: Edited
   "april_ex_001_001":                      "april_ex_001_001",                      # Auto: Edited
   "april_ex_001_smile_001":                "april_ex_001_smile_001",                # Auto: Edited
-  "april_ex_002_001":                      "april_ex_002_001",                      # Auto: Edited
-  "april_ex_003_001":                      "april_ex_003_001",                      # Auto: Edited
-  "april_ex_004_001":                      "april_ex_004_001",                      # Auto: Edited
-  "april_ex_004_open_001":                 "april_ex_004_open_001",                 # Auto: Edited
-  "april_ex_005_001":                      "april_ex_005_001",                      # Auto: Edited
-  "april_ex_005_grin2_001":                "",                                      # Auto: Deleted
-  "april_ex_005_grin_001":                 "",                                      # Auto: Deleted
-  "april_ex_006_001":                      "april_ex_006_001",                      # Auto: Edited
-  "april_ex_007_001":                      "",                                      # Auto: Deleted
-  "april_ex_008_001":                      "",                                      # Auto: Deleted
-  "april_ex_008_smile_001":                "",                                      # Auto: Deleted
-  "april_ex_009_002":                      "april_ex_009_002",                      # Auto: Edited
-  "april_ex_010_002":                      "april_ex_010_002",                      # Auto: Edited
-  "april_ex_011_002":                      "april_ex_011_002",                      # Auto: Edited
-  "april_ex_012_002":                      "april_ex_012_002",                      # Auto: Edited
-  "april_ex_013_002":                      "",                                      # Auto: Deleted
-  "april_ex_014_002":                      "",                                      # Auto: Deleted
-  "april_ex_015_002":                      "",                                      # Auto: Deleted
+  "april_ex_002_001":                      "april_ex_003_001",
+  "april_ex_003_001":                      "april_ex_003_grin_001",
+  "april_ex_004_001":                      "april_ex_003_irked_001",
+  "april_ex_004_open_001":                 "april_ex_003_open_001",
+  "april_ex_005_001":                      "april_ex_003_001",
+  "april_ex_005_grin2_001":                "april_ex_003_grin_001",
+  "april_ex_005_grin_001":                 "april_ex_003_001",
+  "april_ex_006_001":                      "april_ex_003_open2_001",
+  "april_ex_007_001":                      "april_ex_005_aroused2_001",
+  "april_ex_008_001":                      "april_ex_004_open_001",
+  "april_ex_008_smile_001":                "april_ex_004_smile_001",
+  "april_ex_009_002":                      "april_ex_007_002",
+  "april_ex_010_002":                      "april_ex_008_open_002",
+  "april_ex_011_002":                      "april_ex_010_002",
+  "april_ex_012_002":                      "april_ex_009_002",
+  "april_ex_013_002":                      "april_ex_011_002",
+  "april_ex_014_002":                      "april_ex_012_002",
+  "april_ex_015_002":                      "april_ex_009_closed_002",
   "april_ex_male_001_001":                 "april_ex_male_001_001",                 # Auto: Edited
   "april_ex_male_002_001":                 "april_ex_male_002_001",                 # Auto: Edited
   "april_ex_male_003_001":                 "april_ex_male_003_001",                 # Auto: Edited
