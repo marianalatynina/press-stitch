d3d54c19a9bcf664a6800274f1972bbda1eba34b
Mariana <mariana.latynina@gmail.com>
2020-10-20 10:15:15 +0100

    Update Amber graphics manually

--- character_map_amber.py
+++ character_map_amber.py
@@ -15,56 +15,56 @@ characterMapAmber = {
   "amber_ex_003_001":                      "amber_ex_003_001",                      # Auto: Edited
   "amber_ex_003_grin2_001":                "amber_ex_003_grin2_001",                # Auto: Edited
   "amber_ex_003_grin_001":                 "amber_ex_003_grin_001",                 # Auto: Edited
-  "amber_ex_003_irked_001":                "amber_ex_003_irked_001",                # Auto: Edited
-  "amber_ex_003_open2_001":                "amber_ex_003_open2_001",                # Auto: Edited
-  "amber_ex_003_open_001":                 "amber_ex_003_open_001",                 # Auto: Edited
-  "amber_ex_004_001":                      "amber_ex_004_001",                      # Auto: Edited
-  "amber_ex_004_closed_001":               "",                                      # Auto: Deleted
+  "amber_ex_003_irked_001":                "amber_ex_001_smile2_001",
+  "amber_ex_003_open2_001":                "amber_ex_003_grit_001",
+  "amber_ex_003_open_001":                 "amber_ex_003_irked2_001",
+  "amber_ex_004_001":                      "amber_ex_004_open_001",
+  "amber_ex_004_closed_001":               "amber_ex_004_001",
   "amber_ex_004_smile_001":                "amber_ex_004_smile_001",                # Auto: Edited
   "amber_ex_005_001":                      "amber_ex_005_001",                      # Auto: Edited
-  "amber_ex_005_open_001":                 "amber_ex_005_open_001",                 # Auto: Edited
+  "amber_ex_005_open_001":                 "amber_ex_005_open2_001",
   "amber_ex_005_smile2_001":               "amber_ex_005_smile2_001",               # Auto: Edited
   "amber_ex_005_smile_001":                "amber_ex_005_smile_001",                # Auto: Edited
   "amber_ex_006_001":                      "amber_ex_006_001",                      # Auto: Edited
-  "amber_ex_006_open_001":                 "amber_ex_006_open_001",                 # Auto: Edited
+  "amber_ex_006_open_001":                 "amber_ex_006_open2_001",
   "amber_ex_006_smile2_001":               "amber_ex_006_smile2_001",               # Auto: Edited
   "amber_ex_006_smile_001":                "amber_ex_006_smile_001",                # Auto: Edited
-  "amber_ex_007_001":                      "",                                      # Auto: Deleted
+  "amber_ex_007_001":                      "amber_ex_005_aroused2_001",
   "amber_ex_008_002":                      "amber_ex_008_002",                      # Auto: Edited
   "amber_ex_009_002":                      "amber_ex_009_002",                      # Auto: Edited
-  "amber_ex_010_002":                      "amber_ex_010_002",                      # Auto: Edited
-  "amber_ex_011_002":                      "amber_ex_011_002",                      # Auto: Edited
-  "amber_ex_012_002":                      "amber_ex_012_002",                      # Auto: Edited
+  "amber_ex_010_002":                      "amber_ex_009_open_002",
+  "amber_ex_011_002":                      "amber_ex_010_002",                      # Auto: Edited
+  "amber_ex_012_002":                      "amber_ex_011_002",                      # Auto: Edited
   "amber_ex_013_002":                      "amber_ex_013_002",                      # Auto: Edited
-  "amber_ex_014_002":                      "",                                      # Auto: Deleted
-  "amber_ex_full_001_001":                 "amber_ex_full_001_001",                 # Auto: Same
-  "amber_ex_full_001_neutral_001":         "amber_ex_full_001_neutral_001",         # Auto: Same
-  "amber_ex_full_001_smile_001":           "amber_ex_full_001_smile_001",           # Auto: Same
-  "amber_ex_full_002_001":                 "amber_ex_full_002_001",                 # Auto: Same
-  "amber_ex_full_002_smile_001":           "amber_ex_full_002_smile_001",           # Auto: Same
-  "amber_ex_full_003_001":                 "amber_ex_full_003_001",                 # Auto: Same
-  "amber_ex_full_003_grin2_001":           "amber_ex_full_003_grin2_001",           # Auto: Same
-  "amber_ex_full_003_grin_001":            "amber_ex_full_003_grin_001",            # Auto: Same
-  "amber_ex_full_003_irked_001":           "amber_ex_full_001_smile2_001",          # Auto: Renamed
-  "amber_ex_full_003_open2_001":           "amber_ex_full_003_grit_001",            # Auto: Renamed
-  "amber_ex_full_003_open_001":            "amber_ex_full_003_irked2_001",          # Auto: Renamed
-  "amber_ex_full_004_001":                 "amber_ex_full_004_open_001",            # Auto: Renamed
-  "amber_ex_full_004_closed_001":          "amber_ex_full_004_001",                 # Auto: Renamed
-  "amber_ex_full_004_smile_001":           "amber_ex_full_004_smile_001",           # Auto: Same
-  "amber_ex_full_005_001":                 "amber_ex_full_005_001",                 # Auto: Same
-  "amber_ex_full_005_open_001":            "amber_ex_full_005_open_001",            # Auto: Edited
-  "amber_ex_full_005_smile2_001":          "amber_ex_full_005_smile2_001",          # Auto: Same
-  "amber_ex_full_005_smile_001":           "amber_ex_full_005_smile_001",           # Auto: Same
-  "amber_ex_full_006_001":                 "amber_ex_full_006_001",                 # Auto: Same
-  "amber_ex_full_006_open_001":            "amber_ex_full_006_open_001",            # Auto: Edited
-  "amber_ex_full_006_smile2_001":          "amber_ex_full_006_smile2_001",          # Auto: Same
-  "amber_ex_full_006_smile_001":           "amber_ex_full_006_smile_001",           # Auto: Same
-  "amber_ex_full_007_001":                 "amber_ex_full_005_aroused2_001",        # Auto: Renamed
-  "amber_ex_full_008_002":                 "amber_ex_full_008_002",                 # Auto: Same
-  "amber_ex_full_009_002":                 "amber_ex_full_007_open_002",            # Auto: Renamed
-  "amber_ex_full_010_002":                 "amber_ex_full_010_002",                 # Auto: Edited
-  "amber_ex_full_011_002":                 "amber_ex_full_010_002",                 # Auto: Renamed
-  "amber_ex_full_012_002":                 "amber_ex_full_011_002",                 # Auto: Renamed
-  "amber_ex_full_013_002":                 "amber_ex_full_013_002",                 # Auto: Same
-  "amber_ex_full_014_002":                 "amber_ex_full_012_002",                 # Auto: Renamed
+  "amber_ex_014_002":                      "amber_ex_012_002",
+  "amber_ex_full_001_001":                 "amber_ex_full_001_001",
+  "amber_ex_full_001_neutral_001":         "amber_ex_full_001_neutral_001",
+  "amber_ex_full_001_smile_001":           "amber_ex_full_001_smile_001",
+  "amber_ex_full_002_001":                 "amber_ex_full_002_001",
+  "amber_ex_full_002_smile_001":           "amber_ex_full_002_smile_001",
+  "amber_ex_full_003_001":                 "amber_ex_full_003_001",
+  "amber_ex_full_003_grin2_001":           "amber_ex_full_003_grin2_001",
+  "amber_ex_full_003_grin_001":            "amber_ex_full_003_grin_001",
+  "amber_ex_full_003_irked_001":           "amber_ex_full_001_smile2_001",
+  "amber_ex_full_003_open2_001":           "amber_ex_full_003_grit_001",
+  "amber_ex_full_003_open_001":            "amber_ex_full_003_irked2_001",
+  "amber_ex_full_004_001":                 "amber_ex_full_004_open_001",
+  "amber_ex_full_004_closed_001":          "amber_ex_full_004_001",
+  "amber_ex_full_004_smile_001":           "amber_ex_full_004_smile_001",
+  "amber_ex_full_005_001":                 "amber_ex_full_005_001",
+  "amber_ex_full_005_open_001":            "amber_ex_full_005_open2_001",
+  "amber_ex_full_005_smile2_001":          "amber_ex_full_005_smile2_001",
+  "amber_ex_full_005_smile_001":           "amber_ex_full_005_smile_001",
+  "amber_ex_full_006_001":                 "amber_ex_full_006_001",
+  "amber_ex_full_006_open_001":            "amber_ex_full_006_open2_001",
+  "amber_ex_full_006_smile2_001":          "amber_ex_full_006_smile2_001",
+  "amber_ex_full_006_smile_001":           "amber_ex_full_006_smile_001",
+  "amber_ex_full_007_001":                 "amber_ex_full_005_aroused2_001",
+  "amber_ex_full_008_002":                 "amber_ex_full_008_002",
+  "amber_ex_full_009_002":                 "amber_ex_full_009_002",
+  "amber_ex_full_010_002":                 "amber_ex_full_009_open_002",
+  "amber_ex_full_011_002":                 "amber_ex_full_010_002",
+  "amber_ex_full_012_002":                 "amber_ex_full_011_002",
+  "amber_ex_full_013_002":                 "amber_ex_full_013_002",
+  "amber_ex_full_014_002":                 "amber_ex_full_012_002",
 }
