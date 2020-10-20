2f9f358801f4bf58d10caa0fd1a7484999333d58
Mariana <mariana.latynina@gmail.com>
2020-10-20 09:43:59 +0100

    Update Jillian graphics manually

--- character_map_jillian.py
+++ character_map_jillian.py
@@ -38,15 +38,15 @@ characterMapJillian = {
   "jillian_ex_001_001":                    "jillian_ex_001_001",                    # Auto: Edited
   "jillian_ex_001_neutral_001":            "jillian_ex_001_neutral_001",            # Auto: Edited
   "jillian_ex_001_smile_001":              "jillian_ex_001_smile_001",              # Auto: Edited
-  "jillian_ex_002_001":                    "jillian_ex_002_001",                    # Auto: Edited
-  "jillian_ex_002_smile_001":              "jillian_ex_002_smile_001",              # Auto: Edited
-  "jillian_ex_003_001":                    "jillian_ex_003_001",                    # Auto: Edited
-  "jillian_ex_003_grin2_001":              "jillian_ex_003_grin2_001",              # Auto: Edited
-  "jillian_ex_003_grin_001":               "jillian_ex_003_grin_001",               # Auto: Edited
+  "jillian_ex_002_001":                    "jillian_ex_002_smile_001",
+  "jillian_ex_002_smile_001":              "jillian_ex_002_001",
+  "jillian_ex_003_001":                    "jillian_ex_001_neutral_001",            # Not an exact match
+  "jillian_ex_003_grin2_001":              "jillian_ex_001_smile_001",              # Not an exact match
+  "jillian_ex_003_grin_001":               "jillian_ex_001_001",                    # Not an exact match
   "jillian_ex_003_open2_001":              "jillian_ex_003_open2_001",              # Auto: Edited
   "jillian_ex_003_open_001":               "jillian_ex_003_open_001",               # Auto: Edited
-  "jillian_ex_004_001":                    "jillian_ex_004_001",                    # Auto: Edited
-  "jillian_ex_004_closed_001":             "",                                      # Auto: Deleted
+  "jillian_ex_004_001":                    "jillian_ex_004_open_001",
+  "jillian_ex_004_closed_001":             "jillian_ex_004_001",
   "jillian_ex_004_smile_001":              "jillian_ex_004_smile_001",              # Auto: Edited
   "jillian_ex_005_001":                    "jillian_ex_005_001",                    # Auto: Edited
   "jillian_ex_005_open_001":               "jillian_ex_005_open_001",               # Auto: Edited
@@ -56,47 +56,47 @@ characterMapJillian = {
   "jillian_ex_006_open_001":               "jillian_ex_006_open_001",               # Auto: Edited
   "jillian_ex_006_smile2_001":             "jillian_ex_006_smile2_001",             # Auto: Edited
   "jillian_ex_006_smile_001":              "jillian_ex_006_smile_001",              # Auto: Edited
-  "jillian_ex_007_001":                    "",                                      # Auto: Deleted
-  "jillian_ex_008_001":                    "",                                      # Auto: Deleted
-  "jillian_ex_009_001":                    "",                                      # Auto: Deleted
-  "jillian_ex_010_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_011_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_012_002":                    "jillian_ex_012_002",                    # Auto: Edited
-  "jillian_ex_013_002":                    "jillian_ex_013_002",                    # Auto: Edited
-  "jillian_ex_014_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_015_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_016_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_017_002":                    "",                                      # Auto: Deleted
-  "jillian_ex_full_001_001":               "jillian_ex_full_001_001",               # Auto: Edited
-  "jillian_ex_full_001_neutral_001":       "jillian_ex_full_001_neutral_001",       # Auto: Edited
-  "jillian_ex_full_001_smile_001":         "jillian_ex_full_001_smile_001",         # Auto: Edited
-  "jillian_ex_full_002_001":               "jillian_ex_full_002_001",               # Auto: Edited
-  "jillian_ex_full_002_smile_001":         "jillian_ex_full_002_smile_001",         # Auto: Edited
-  "jillian_ex_full_003_001":               "jillian_ex_full_003_001",               # Auto: Edited
-  "jillian_ex_full_003_grin2_001":         "jillian_ex_full_003_grin2_001",         # Auto: Edited
-  "jillian_ex_full_003_grin_001":          "jillian_ex_full_003_grin_001",          # Auto: Edited
-  "jillian_ex_full_003_open2_001":         "jillian_ex_full_003_open2_001",         # Auto: Edited
-  "jillian_ex_full_003_open_001":          "jillian_ex_full_003_open_001",          # Auto: Edited
-  "jillian_ex_full_004_001":               "jillian_ex_full_004_001",               # Auto: Edited
-  "jillian_ex_full_004_closed_001":        "",                                      # Auto: Deleted
-  "jillian_ex_full_004_smile_001":         "jillian_ex_full_004_smile_001",         # Auto: Edited
-  "jillian_ex_full_005_001":               "jillian_ex_full_005_001",               # Auto: Edited
-  "jillian_ex_full_005_open_001":          "jillian_ex_full_005_open_001",          # Auto: Edited
-  "jillian_ex_full_005_smile2_001":        "jillian_ex_full_005_smile2_001",        # Auto: Edited
-  "jillian_ex_full_005_smile_001":         "jillian_ex_full_005_smile_001",         # Auto: Edited
-  "jillian_ex_full_006_001":               "jillian_ex_full_006_001",               # Auto: Edited
-  "jillian_ex_full_006_open_001":          "jillian_ex_full_006_open_001",          # Auto: Edited
-  "jillian_ex_full_006_smile2_001":        "jillian_ex_full_006_smile2_001",        # Auto: Edited
-  "jillian_ex_full_006_smile_001":         "jillian_ex_full_006_smile_001",         # Auto: Edited
-  "jillian_ex_full_007_001":               "",                                      # Auto: Deleted
-  "jillian_ex_full_008_001":               "",                                      # Auto: Deleted
-  "jillian_ex_full_009_001":               "",                                      # Auto: Deleted
-  "jillian_ex_full_010_002":               "",                                      # Auto: Deleted
-  "jillian_ex_full_011_002":               "",                                      # Auto: Deleted
-  "jillian_ex_full_012_002":               "jillian_ex_full_012_002",               # Auto: Edited
-  "jillian_ex_full_013_002":               "jillian_ex_full_013_002",               # Auto: Edited
-  "jillian_ex_full_014_002":               "",                                      # Auto: Deleted
-  "jillian_ex_full_015_002":               "",                                      # Auto: Deleted
-  "jillian_ex_full_016_002":               "",                                      # Auto: Deleted
-  "jillian_ex_full_017_002":               "",                                      # Auto: Deleted
+  "jillian_ex_007_001":                    "jillian_ex_010_001",
+  "jillian_ex_008_001":                    "jillian_ex_011_001",
+  "jillian_ex_009_001":                    "jillian_ex_005_aroused2_001",
+  "jillian_ex_010_002":                    "jillian_ex_013_smile_002",
+  "jillian_ex_011_002":                    "jillian_ex_013_002",
+  "jillian_ex_012_002":                    "jillian_ex_009_002",                    # Auto: Edited
+  "jillian_ex_013_002":                    "jillian_ex_008_002",                    # Auto: Edited
+  "jillian_ex_014_002":                    "jillian_ex_008_open_002",
+  "jillian_ex_015_002":                    "jillian_ex_012_002",
+  "jillian_ex_016_002":                    "jillian_ex_012_open_002",
+  "jillian_ex_017_002":                    "jillian_ex_012_smile_002",
+  "jillian_ex_full_001_001":               "jillian_ex_full_001_001",
+  "jillian_ex_full_001_neutral_001":       "jillian_ex_full_001_neutral_001",
+  "jillian_ex_full_001_smile_001":         "jillian_ex_full_001_smile_001",
+  "jillian_ex_full_002_001":               "jillian_ex_full_002_smile_001",
+  "jillian_ex_full_002_smile_001":         "jillian_ex_full_002_001",
+  "jillian_ex_full_003_001":               "jillian_ex_full_001_neutral_001",
+  "jillian_ex_full_003_grin2_001":         "jillian_ex_full_001_smile_001",
+  "jillian_ex_full_003_grin_001":          "jillian_ex_full_001_001",
+  "jillian_ex_full_003_open2_001":         "jillian_ex_full_003_open2_001",
+  "jillian_ex_full_003_open_001":          "jillian_ex_full_003_open_001",
+  "jillian_ex_full_004_001":               "jillian_ex_full_004_open_001",
+  "jillian_ex_full_004_closed_001":        "jillian_ex_full_004_001",
+  "jillian_ex_full_004_smile_001":         "jillian_ex_full_004_smile_001",
+  "jillian_ex_full_005_001":               "jillian_ex_full_005_001",
+  "jillian_ex_full_005_open_001":          "jillian_ex_full_005_open_001",
+  "jillian_ex_full_005_smile2_001":        "jillian_ex_full_005_smile2_001",
+  "jillian_ex_full_005_smile_001":         "jillian_ex_full_005_smile_001",
+  "jillian_ex_full_006_001":               "jillian_ex_full_006_001",
+  "jillian_ex_full_006_open_001":          "jillian_ex_full_006_open_001",
+  "jillian_ex_full_006_smile2_001":        "jillian_ex_full_006_smile2_001",
+  "jillian_ex_full_006_smile_001":         "jillian_ex_full_006_smile_001",
+  "jillian_ex_full_007_001":               "jillian_ex_full_010_001",
+  "jillian_ex_full_008_001":               "jillian_ex_full_011_001",
+  "jillian_ex_full_009_001":               "jillian_ex_full_005_aroused2_001",
+  "jillian_ex_full_010_002":               "jillian_ex_full_013_smile_002",
+  "jillian_ex_full_011_002":               "jillian_ex_full_013_002",
+  "jillian_ex_full_012_002":               "jillian_ex_full_009_002",
+  "jillian_ex_full_013_002":               "jillian_ex_full_008_002",
+  "jillian_ex_full_014_002":               "jillian_ex_full_008_open_002",
+  "jillian_ex_full_015_002":               "jillian_ex_full_012_002",
+  "jillian_ex_full_016_002":               "jillian_ex_full_012_open_002",
+  "jillian_ex_full_017_002":               "jillian_ex_full_012_smile_002",
 }
