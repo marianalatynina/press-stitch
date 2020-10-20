6ac67b8e4ffd07f5161e286949e437887f3cf4ae
Mariana <mariana.latynina@gmail.com>
2020-10-20 15:47:51 +0100

    Complete Ciel's graphics

--- character_map_ciel.py
+++ character_map_ciel.py
@@ -22,7 +22,7 @@ characterMapCiel = {
   "ciel_ex_012_002":                       "ciel_ex_011_002",                       # Auto: Edited
   "ciel_ex_013_002":                       "ciel_ex_012_002",                       # Manual
   "ciel_ex_014_003":                       "ciel_ex_005_aroused_003",               # Manual
-  "ciel_ex_015_003":                       "",                                      # Auto: Deleted
+  "ciel_ex_015_003":                       "ciel_ex_005_aroused2_003",
   "ciel_ex_016_003":                       "ciel_ex_005_aroused3_003",              # Manual
   "ciel_ex_full_001_001":                  "ciel_ex_full_001_001",                  # Auto: Edited
   "ciel_ex_full_002_001":                  "ciel_ex_full_002_001",                  # Auto: Edited
@@ -30,14 +30,14 @@ characterMapCiel = {
   "ciel_ex_full_004_001":                  "ciel_ex_full_004_001",                  # Auto: Edited
   "ciel_ex_full_005_001":                  "ciel_ex_full_005_001",                  # Auto: Edited
   "ciel_ex_full_006_001":                  "ciel_ex_full_006_001",                  # Auto: Edited
-  "ciel_ex_full_007_001":                  "",                                      # Auto: Deleted
+  "ciel_ex_full_007_001":                  "ciel_ex_full_001_open_001",
   "ciel_ex_full_008_002":                  "ciel_ex_full_008_002",                  # Auto: Edited
   "ciel_ex_full_009_002":                  "ciel_ex_full_009_002",                  # Auto: Edited
   "ciel_ex_full_010_002":                  "ciel_ex_full_010_002",                  # Auto: Edited
   "ciel_ex_full_011_002":                  "ciel_ex_full_011_002",                  # Auto: Edited
   "ciel_ex_full_012_002":                  "ciel_ex_full_012_002",                  # Auto: Edited
-  "ciel_ex_full_013_002":                  "",                                      # Auto: Deleted
-  "ciel_ex_full_014_003":                  "",                                      # Auto: Deleted
+  "ciel_ex_full_013_002":                  "ciel_ex_full_005_aroused_003",
+  "ciel_ex_full_014_003":                  "ciel_ex_full_005_aroused2_003",
   "ciel_ex_full_015_003":                  "ciel_ex_full_005_aroused2_003",         # Manual
-  "ciel_ex_full_016_003":                  "",                                      # Auto: Deleted
+  "ciel_ex_full_016_003":                  "ciel_ex_full_005_aroused3_003",
 }
