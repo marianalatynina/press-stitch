a5ae19e06be7065c6684701122163e3f10d2bb9f
Mariana <mariana.latynina@gmail.com>
2020-10-22 01:37:28 +0100

    Update Melina graphics manually

--- character_map_melina.py
+++ character_map_melina.py
@@ -11,36 +11,36 @@ characterMapMelina = {
   "melina_base_naked_002":                 "melina_base_naked_002",                 # Auto: Edited
   "melina_ex_001_001":                     "melina_ex_001_001",                     # Auto: Edited
   "melina_ex_001_smile_001":               "melina_ex_001_smile_001",               # Auto: Edited
-  "melina_ex_002_001":                     "melina_ex_002_001",                     # Auto: Edited
-  "melina_ex_003_001":                     "melina_ex_003_001",                     # Auto: Edited
-  "melina_ex_004_001":                     "melina_ex_004_001",                     # Auto: Edited
-  "melina_ex_004_grin2_001":               "",                                      # Auto: Deleted
-  "melina_ex_004_grin_001":                "",                                      # Auto: Deleted
-  "melina_ex_004_open_001":                "melina_ex_004_open_001",                # Auto: Edited
-  "melina_ex_005_001":                     "melina_ex_005_001",                     # Auto: Edited
-  "melina_ex_006_001":                     "melina_ex_006_001",                     # Auto: Edited
-  "melina_ex_007_001":                     "",                                      # Auto: Deleted
-  "melina_ex_008_002":                     "melina_ex_008_002",                     # Auto: Edited
-  "melina_ex_009_002":                     "melina_ex_009_002",                     # Auto: Edited
+  "melina_ex_002_001":                     "melina_ex_001_neutral_001",
+  "melina_ex_003_001":                     "melina_ex_002_smile_001",
+  "melina_ex_004_001":                     "melina_ex_003_001",
+  "melina_ex_004_grin2_001":               "melina_ex_003_grin2_001",               # Not exact, but close
+  "melina_ex_004_grin_001":                "melina_ex_003_grin_001",
+  "melina_ex_004_open_001":                "melina_ex_003_open_001",
+  "melina_ex_005_001":                     "melina_ex_003_open2_001",
+  "melina_ex_006_001":                     "melina_ex_005_smile2_001",
+  "melina_ex_007_001":                     "melina_ex_004_open_001",
+  "melina_ex_008_002":                     "melina_ex_007_open_002",
+  "melina_ex_009_002":                     "melina_ex_008_open_002",
   "melina_ex_010_002":                     "melina_ex_010_002",                     # Auto: Edited
-  "melina_ex_011_002":                     "melina_ex_011_002",                     # Auto: Edited
-  "melina_ex_012_002":                     "melina_ex_012_002",                     # Auto: Edited
-  "melina_ex_013_002":                     "",                                      # Auto: Deleted
-  "melina_ex_014_002":                     "",                                      # Auto: Deleted
+  "melina_ex_011_002":                     "melina_ex_009_002",
+  "melina_ex_012_002":                     "melina_ex_009_closed2_002",
+  "melina_ex_013_002":                     "melina_ex_011_002",
+  "melina_ex_014_002":                     "melina_ex_012_002",
   "melina_ex_full_001_001":                "melina_ex_full_001_001",                # Auto: Edited
-  "melina_ex_full_002_001":                "melina_ex_full_002_001",                # Auto: Edited
-  "melina_ex_full_003_001":                "melina_ex_full_003_001",                # Auto: Edited
-  "melina_ex_full_004_001":                "melina_ex_full_004_001",                # Auto: Edited
-  "melina_ex_full_005_001":                "melina_ex_full_005_001",                # Auto: Edited
-  "melina_ex_full_006_001":                "melina_ex_full_006_001",                # Auto: Edited
-  "melina_ex_full_007_001":                "",                                      # Auto: Deleted
-  "melina_ex_full_008_002":                "melina_ex_full_008_002",                # Auto: Edited
-  "melina_ex_full_009_002":                "melina_ex_full_009_002",                # Auto: Edited
+  "melina_ex_full_002_001":                "melina_ex_full_001_neutral_001",
+  "melina_ex_full_003_001":                "melina_ex_full_002_smile_001",
+  "melina_ex_full_004_001":                "melina_ex_full_003_001",
+  "melina_ex_full_005_001":                "melina_ex_full_003_open2_001",
+  "melina_ex_full_006_001":                "melina_ex_full_005_aroused2_001",       # Not exact
+  "melina_ex_full_007_001":                "melina_ex_full_004_open_001",
+  "melina_ex_full_008_002":                "melina_ex_full_007_open_002",
+  "melina_ex_full_009_002":                "melina_ex_full_008_open_002",
   "melina_ex_full_010_002":                "melina_ex_full_010_002",                # Auto: Edited
-  "melina_ex_full_011_002":                "melina_ex_full_011_002",                # Auto: Edited
-  "melina_ex_full_012_002":                "melina_ex_full_012_002",                # Auto: Edited
-  "melina_ex_full_013_002":                "",                                      # Auto: Deleted
-  "melina_ex_full_014_002":                "",                                      # Auto: Deleted
+  "melina_ex_full_011_002":                "melina_ex_full_009_002",
+  "melina_ex_full_012_002":                "melina_ex_full_09_closed2_002",
+  "melina_ex_full_013_002":                "melina_ex_full_011_002",
+  "melina_ex_full_014_002":                "melina_ex_full_012_002",
   "melina_mut_headband_001":               "",                                      # Auto: Deleted
   "melina_mut_headband_002":               "",                                      # Auto: Deleted
   "melina_mut_naked_bra_001":              "",                                      # Auto: Deleted
