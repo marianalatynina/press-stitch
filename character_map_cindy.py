3b9ebad405df312c6474805ce8cad8543ba45758
Mariana <mariana.latynina@gmail.com>
2020-10-22 01:08:22 +0100

    Update Cindy graphics manually

--- character_map_cindy.py
+++ character_map_cindy.py
@@ -2,17 +2,17 @@ characterMapCindy = {
   "cindy_base_001":                        "cindy_base_001",                        # Auto: Edited
   "cindy_base_002":                        "cindy_base_002",                        # Auto: Edited
   "cindy_ex_001_001":                      "cindy_ex_001_001",                      # Auto: Edited
-  "cindy_ex_002_001":                      "cindy_ex_002_001",                      # Auto: Edited
-  "cindy_ex_003_001":                      "cindy_ex_003_001",                      # Auto: Edited
-  "cindy_ex_004_001":                      "cindy_ex_004_001",                      # Auto: Edited
-  "cindy_ex_005_001":                      "cindy_ex_005_001",                      # Auto: Edited
-  "cindy_ex_006_001":                      "cindy_ex_006_001",                      # Auto: Edited
-  "cindy_ex_007_001":                      "",                                      # Auto: Deleted
-  "cindy_ex_008_002":                      "cindy_ex_008_002",                      # Auto: Edited
-  "cindy_ex_009_002":                      "cindy_ex_009_002",                      # Auto: Edited
+  "cindy_ex_002_001":                      "cindy_ex_001_neutral_001",
+  "cindy_ex_003_001":                      "cindy_ex_002_smile_001",
+  "cindy_ex_004_001":                      "cindy_ex_003_grit_001",
+  "cindy_ex_005_001":                      "cindy_ex_003_open2_001",
+  "cindy_ex_006_001":                      "cindy_ex_005_smile_001",
+  "cindy_ex_007_001":                      "cindy_ex_004_open_001",
+  "cindy_ex_008_002":                      "cindy_ex_007_002",
+  "cindy_ex_009_002":                      "cindy_ex_008_open_002",
   "cindy_ex_010_002":                      "cindy_ex_010_002",                      # Auto: Edited
-  "cindy_ex_011_002":                      "cindy_ex_011_002",                      # Auto: Edited
-  "cindy_ex_012_002":                      "cindy_ex_012_002",                      # Auto: Edited
-  "cindy_ex_013_002":                      "",                                      # Auto: Deleted
-  "cindy_ex_014_002":                      "",                                      # Auto: Deleted
+  "cindy_ex_011_002":                      "cindy_ex_009_002",
+  "cindy_ex_012_002":                      "cindy_ex_011_002",
+  "cindy_ex_013_002":                      "cindy_ex_012_002",
+  "cindy_ex_014_002":                      "cindy_ex_009_closed_002",
 }
