3c8cf7ddc121600460692b78390f58adabd784f5
Mariana <mariana.latynina@gmail.com>
2021-04-13 23:57:23 +0100

    Update Eliza and Mother mappings for 0.3

--- character_map_35_mother.py
+++ character_map_35_mother.py
@@ -11,39 +11,39 @@ characterMapMother = {
   "mother_base_naked_002":                 "mother_base_naked_002",                 # Auto: Edited
   "mother_base_suit_001":                  "mother_base_suit_001",                  # Auto: Edited
   "mother_base_suit_002":                  "mother_base_suit_002",                  # Auto: Edited
-  "mother_ex_001":                         "mother_ex_001_001",                     # Auto: Edited and renamed
-  "mother_ex_002":                         "mother_ex_002_002",                     # Auto: Edited and renamed
-  "mother_ex_002_smile":                   "mother_ex_002_smile_002",               # Auto: Edited and renamed
-  "mother_ex_002_smile2":                  "",                                      # Auto: Deleted
-  "mother_ex_003":                         "mother_ex_003_001",                     # Auto: Edited and renamed
-  "mother_ex_004":                         "mother_ex_004_002",                     # Auto: Edited and renamed
-  "mother_ex_004_grin":                    "",                                      # Auto: Deleted
-  "mother_ex_004_grin2":                   "",                                      # Auto: Deleted
-  "mother_ex_004_open":                    "mother_ex_004_open_002",                # Auto: Edited and renamed
-  "mother_ex_005":                         "mother_ex_005_002",                     # Auto: Edited and renamed
-  "mother_ex_006":                         "mother_ex_006_002",                     # Auto: Edited and renamed
-  "mother_ex_007":                         "mother_ex_007_002",                     # Auto: Edited and renamed
-  "mother_ex_008":                         "mother_ex_008_002",                     # Auto: Edited and renamed
-  "mother_ex_009":                         "mother_ex_009_002",                     # Auto: Edited and renamed
-  "mother_ex_010":                         "mother_ex_010_002",                     # Auto: Edited and renamed
-  "mother_ex_011":                         "mother_ex_011_002",                     # Auto: Edited and renamed
-  "mother_ex_012":                         "mother_ex_012_002",                     # Auto: Edited and renamed
-  "mother_ex_013":                         "",                                      # Auto: Deleted
-  "mother_ex_014":                         "",                                      # Auto: Deleted
-  "mother_ex_full_001":                    "mother_ex_full_001_001",                # Auto: Edited and renamed
-  "mother_ex_full_002":                    "mother_ex_full_002_002",                # Auto: Edited and renamed
-  "mother_ex_full_003":                    "mother_ex_full_003_001",                # Auto: Edited and renamed
-  "mother_ex_full_004":                    "mother_ex_full_004_002",                # Auto: Edited and renamed
-  "mother_ex_full_005":                    "mother_ex_full_005_002",                # Auto: Edited and renamed
-  "mother_ex_full_006":                    "mother_ex_full_006_002",                # Auto: Edited and renamed
-  "mother_ex_full_007":                    "mother_ex_full_007_002",                # Auto: Edited and renamed
-  "mother_ex_full_008":                    "mother_ex_full_008_002",                # Auto: Edited and renamed
-  "mother_ex_full_009":                    "mother_ex_full_009_002",                # Auto: Edited and renamed
-  "mother_ex_full_010":                    "mother_ex_full_010_002",                # Auto: Edited and renamed
-  "mother_ex_full_010_eyes":               "",                                      # Auto: Deleted
-  "mother_ex_full_010_smile":              "",                                      # Auto: Deleted
-  "mother_ex_full_011":                    "mother_ex_full_011_002",                # Auto: Edited and renamed
-  "mother_ex_full_012":                    "mother_ex_full_012_002",                # Auto: Edited and renamed
-  "mother_ex_full_013":                    "",                                      # Auto: Deleted
-  "mother_ex_full_014":                    "",                                      # Auto: Deleted
+  "mother_ex_001":                         "mother_ex_001_neutral_001",
+  "mother_ex_002":                         "mother_ex_001_001",
+  "mother_ex_002_smile":                   "mother_ex_001_smile2_001",
+  "mother_ex_002_smile2":                  "mother_ex_001_smile_001",               # Not an exact match
+  "mother_ex_003":                         "mother_ex_001_smile2_001",              # Not an exact match
+  "mother_ex_004":                         "mother_ex_003_002",
+  "mother_ex_004_grin":                    "mother_ex_003_grin_002",
+  "mother_ex_004_grin2":                   "mother_ex_003_grin3_002",
+  "mother_ex_004_open":                    "mother_ex_003_002",
+  "mother_ex_005":                         "mother_ex_003_open2_002",
+  "mother_ex_006":                         "mother_ex_003_irked2_002",              # Not an exact match
+  "mother_ex_007":                         "mother_ex_003_irked_002",
+  "mother_ex_008":                         "mother_ex_005_open2_002",
+  "mother_ex_009":                         "mother_ex_008_open2_002",
+  "mother_ex_010":                         "mother_ex_010_002",
+  "mother_ex_011":                         "mother_ex_009_002",
+  "mother_ex_012":                         "mother_ex_012_002",
+  "mother_ex_013":                         "mother_ex_011_002",
+  "mother_ex_014":                         "mother_ex_009_closed_002",
+  "mother_ex_full_001":                    "mother_ex_full_001_neutral_001",
+  "mother_ex_full_002":                    "mother_ex_full_001_001",
+  "mother_ex_full_003":                    "mother_ex_full_001_smile2_001",
+  "mother_ex_full_004":                    "mother_ex_full_003_002",
+  "mother_ex_full_005":                    "mother_ex_full_003_open2_002",
+  "mother_ex_full_006":                    "mother_ex_full_003_irked2_002",         # Not an exact match
+  "mother_ex_full_007":                    "mother_ex_full_003_irked_002",
+  "mother_ex_full_008":                    "mother_ex_full_005_open2_002",
+  "mother_ex_full_009":                    "mother_ex_full_008_open2_002",
+  "mother_ex_full_010":                    "mother_ex_full_010_002",
+  "mother_ex_full_010_eyes":               "",
+  "mother_ex_full_010_smile":              "mother_ex_full_004_smile_002",
+  "mother_ex_full_011":                    "mother_ex_full_009_002",
+  "mother_ex_full_012":                    "mother_ex_full_012_002",
+  "mother_ex_full_013":                    "mother_ex_full_011_002",
+  "mother_ex_full_014":                    "mother_ex_full_009_closed_002",
 }
