c0540f65a58d2df6c2b5c77a453797338f0c8ed7
Mariana <mariana.latynina@gmail.com>
2021-04-20 21:02:52 +0100

    Update Eliza mapping for 0.5 to 0.6

--- character_map_56_eliza.py
+++ character_map_56_eliza.py
@@ -1,101 +1,100 @@
 characterMapEliza = {
-  "eliza_ex_001_001":               "eliza_ex_001_001",
-  "eliza_ex_001_empty_001":         "eliza_ex_001_empty_001",
-  "eliza_ex_001_neutral_001":       "eliza_ex_001_neutral_001",
-  "eliza_ex_001_open_001":          "eliza_ex_001_open_001",
-  "eliza_ex_001_smile_001":         "eliza_ex_001_smile_001",
-  "eliza_ex_001_smile2_001":        "eliza_ex_001_smile2_001",
-  "eliza_ex_002_001":               "eliza_ex_002_001",
-  "eliza_ex_002_open_001":          "eliza_ex_002_open_001",
-  "eliza_ex_002_smile_001":         "eliza_ex_002_smile_001",
-  "eliza_ex_003_001":               "eliza_ex_003_001",
-  "eliza_ex_003_grin_001":          "eliza_ex_003_grin_001",
-  "eliza_ex_003_grin2_001":         "eliza_ex_003_grin2_001",
-  "eliza_ex_003_grit_001":          "eliza_ex_003_grit_001",
-  "eliza_ex_003_irked_003":         "eliza_ex_003_irked_003",
-  "eliza_ex_003_irked2_003":        "eliza_ex_003_irked2_003",
-  "eliza_ex_003_open_001":          "eliza_ex_003_open_001",
-  "eliza_ex_003_open2_001":         "eliza_ex_003_open2_001",
-  "eliza_ex_004_001":               "eliza_ex_004_001",
-  "eliza_ex_004_open_001":          "eliza_ex_004_open_001",
-  "eliza_ex_004_open2_001":         "eliza_ex_004_open2_001",
-  "eliza_ex_004_smile_001":         "eliza_ex_004_smile_001",
-  "eliza_ex_005_001":               "eliza_ex_005_001",
-  "eliza_ex_005_aroused_001":       "eliza_ex_005_aroused_001",
-  "eliza_ex_005_aroused2_001":      "eliza_ex_005_aroused2_001",
-  "eliza_ex_005_open_001":          "eliza_ex_005_open_001",
-  "eliza_ex_005_open2_001":         "eliza_ex_005_open2_001",
-  "eliza_ex_005_smile_001":         "eliza_ex_005_smile_001",
-  "eliza_ex_005_smile2_001":        "eliza_ex_005_smile2_001",
-  "eliza_ex_006_001":               "eliza_ex_006_001",
-  "eliza_ex_006_open_001":          "eliza_ex_006_open_001",
-  "eliza_ex_006_open2_001":         "eliza_ex_006_open2_001",
-  "eliza_ex_006_smile_001":         "eliza_ex_006_smile_001",
-  "eliza_ex_006_smile2_001":        "eliza_ex_006_smile2_001",
-  "eliza_ex_007_003":               "eliza_ex_007_003",
-  "eliza_ex_007_open_003":          "eliza_ex_007_open_003",
-  "eliza_ex_008_003":               "eliza_ex_008_003",
-  "eliza_ex_008_open_003":          "eliza_ex_008_open_003",
-  "eliza_ex_009_002":               "eliza_ex_009_002",
-  "eliza_ex_009_closed_002":        "eliza_ex_009_closed_002",
-  "eliza_ex_009_open_003":          "eliza_ex_009_open_003",
-  "eliza_ex_010_002":               "eliza_ex_010_002",
-  "eliza_ex_011_003":               "eliza_ex_011_003",
-  "eliza_ex_012_002":               "eliza_ex_012_002",
-  "eliza_ex_013_002":               "eliza_ex_013_002",
-  "eliza_ex_014_002":               "eliza_ex_014_002",
-  "eliza_ex_014_daze_002":          "eliza_ex_014_daze_002",
-  "eliza_ex_014_daze2_002":         "eliza_ex_014_daze2_002",
-  "eliza_ex_015_002":               "eliza_ex_015_002",
-  "eliza_ex_016_003":               "eliza_ex_016_003",
-  "eliza_ex_full_001_001":          "eliza_ex_full_001_001",
-  "eliza_ex_full_001_empty_001":    "eliza_ex_full_001_empty_001",
-  "eliza_ex_full_001_neutral_001":  "eliza_ex_full_001_neutral_001",
-  "eliza_ex_full_001_open_001":     "eliza_ex_full_001_open_001",
-  "eliza_ex_full_001_smile_001":    "eliza_ex_full_001_smile_001",
-  "eliza_ex_full_001_smile2_001":   "eliza_ex_full_001_smile2_001",
-  "eliza_ex_full_002_001":          "eliza_ex_full_002_001",
-  "eliza_ex_full_002_open_001":     "eliza_ex_full_002_open_001",
-  "eliza_ex_full_002_smile_001":    "eliza_ex_full_002_smile_001",
-  "eliza_ex_full_003_001":          "eliza_ex_full_003_001",
-  "eliza_ex_full_003_grin_001":     "eliza_ex_full_003_grin_001",
-  "eliza_ex_full_003_grin2_001":    "eliza_ex_full_003_grin2_001",
-  "eliza_ex_full_003_grit_001":     "eliza_ex_full_003_grit_001",
-  "eliza_ex_full_003_irked_003":    "eliza_ex_full_003_irked_003",
-  "eliza_ex_full_003_irked2_003":   "eliza_ex_full_003_irked2_003",
-  "eliza_ex_full_003_open_001":     "eliza_ex_full_003_open_001",
-  "eliza_ex_full_003_open2_001":    "eliza_ex_full_003_open2_001",
-  "eliza_ex_full_004_001":          "eliza_ex_full_004_001",
-  "eliza_ex_full_004_open_001":     "eliza_ex_full_004_open_001",
-  "eliza_ex_full_004_open2_001":    "eliza_ex_full_004_open2_001",
-  "eliza_ex_full_004_smile_001":    "eliza_ex_full_004_smile_001",
-  "eliza_ex_full_005_001":          "eliza_ex_full_005_001",
-  "eliza_ex_full_005_aroused_001":  "eliza_ex_full_005_aroused_001",
-  "eliza_ex_full_005_aroused2_001": "eliza_ex_full_005_aroused2_001",
-  "eliza_ex_full_005_open_001":     "eliza_ex_full_005_open_001",
-  "eliza_ex_full_005_open2_001":    "eliza_ex_full_005_open2_001",
-  "eliza_ex_full_005_smile_001":    "eliza_ex_full_005_smile_001",
-  "eliza_ex_full_005_smile2_001":   "eliza_ex_full_005_smile2_001",
-  "eliza_ex_full_006_001":          "eliza_ex_full_006_001",
-  "eliza_ex_full_006_open_001":     "eliza_ex_full_006_open_001",
-  "eliza_ex_full_006_open2_001":    "eliza_ex_full_006_open2_001",
-  "eliza_ex_full_006_smile_001":    "eliza_ex_full_006_smile_001",
-  "eliza_ex_full_006_smile2_001":   "eliza_ex_full_006_smile2_001",
-  "eliza_ex_full_007_003":          "eliza_ex_full_007_003",
-  "eliza_ex_full_007_open_003":     "eliza_ex_full_007_open_003",
-  "eliza_ex_full_008_003":          "eliza_ex_full_008_003",
-  "eliza_ex_full_008_open_003":     "eliza_ex_full_008_open_003",
-  "eliza_ex_full_009_002":          "eliza_ex_full_009_002",
-  "eliza_ex_full_009_closed_002":   "eliza_ex_full_009_closed_002",
-  "eliza_ex_full_009_open_003":     "eliza_ex_full_009_open_003",
-  "eliza_ex_full_010_002":          "eliza_ex_full_010_002",
-  "eliza_ex_full_011_003":          "eliza_ex_full_011_003",
-  "eliza_ex_full_012_002":          "eliza_ex_full_012_002",
-  "eliza_ex_full_013_002":          "eliza_ex_full_013_002",
-  "eliza_ex_full_014_002":          "eliza_ex_full_014_002",
-  "eliza_ex_full_014_daze_002":     "eliza_ex_full_014_daze_002",
-  "eliza_ex_full_014_daze2_002":    "eliza_ex_full_014_daze2_002",
-  "eliza_ex_full_015_002":          "eliza_ex_full_015_002",
-  "eliza_ex_full_016_003":          "eliza_ex_full_016_003",
+  "eliza_ex_001_001":                      "eliza_ex_001_001",                      # Auto: Edited
+  "eliza_ex_001_empty_001":                "eliza_ex_001_empty_001",                # Auto: Edited
+  "eliza_ex_001_neutral_001":              "eliza_ex_001_neutral_001",              # Auto: Edited
+  "eliza_ex_001_open_001":                 "eliza_ex_001_open_001",                 # Auto: Edited
+  "eliza_ex_001_smile2_001":               "eliza_ex_001_smile2_001",               # Auto: Edited
+  "eliza_ex_001_smile_001":                "eliza_ex_001_smile_001",                # Auto: Edited
+  "eliza_ex_002_001":                      "eliza_ex_002_001",                      # Auto: Edited
+  "eliza_ex_002_open_001":                 "eliza_ex_002_open_001",                 # Auto: Edited
+  "eliza_ex_002_smile_001":                "eliza_ex_002_smile_001",                # Auto: Edited
+  "eliza_ex_003_001":                      "eliza_ex_003_001",                      # Auto: Edited
+  "eliza_ex_003_grin2_001":                "eliza_ex_003_grin2_001",                # Auto: Edited
+  "eliza_ex_003_grin_001":                 "eliza_ex_003_grin_001",                 # Auto: Edited
+  "eliza_ex_003_grit_001":                 "eliza_ex_003_grit_001",                 # Auto: Edited
+  "eliza_ex_003_irked2_003":               "eliza_ex_003_irked4_003",
+  "eliza_ex_003_irked_003":                "eliza_ex_003_irked3_003",
+  "eliza_ex_003_open2_001":                "eliza_ex_003_open2_001",                # Auto: Edited
+  "eliza_ex_003_open_001":                 "eliza_ex_003_open_001",                 # Auto: Edited
+  "eliza_ex_004_001":                      "eliza_ex_004_001",                      # Auto: Edited
+  "eliza_ex_004_open2_001":                "eliza_ex_004_open2_001",                # Auto: Edited
+  "eliza_ex_004_open_001":                 "eliza_ex_004_open_001",                 # Auto: Edited
+  "eliza_ex_004_smile_001":                "eliza_ex_004_smile_001",                # Auto: Edited
+  "eliza_ex_005_001":                      "eliza_ex_005_001",                      # Auto: Edited
+  "eliza_ex_005_aroused2_001":             "eliza_ex_005_aroused2_001",             # Auto: Edited
+  "eliza_ex_005_aroused_001":              "eliza_ex_005_aroused_001",              # Auto: Edited
+  "eliza_ex_005_open2_001":                "eliza_ex_005_open2_001",                # Auto: Edited
+  "eliza_ex_005_open_001":                 "eliza_ex_005_open_001",                 # Auto: Edited
+  "eliza_ex_005_smile2_001":               "eliza_ex_005_smile2_001",               # Auto: Edited
+  "eliza_ex_005_smile_001":                "eliza_ex_005_smile_001",                # Auto: Edited
+  "eliza_ex_006_001":                      "eliza_ex_006_001",                      # Auto: Edited
+  "eliza_ex_006_open2_001":                "eliza_ex_006_open2_001",                # Auto: Edited
+  "eliza_ex_006_open_001":                 "eliza_ex_006_open_001",                 # Auto: Edited
+  "eliza_ex_006_smile2_001":               "eliza_ex_006_smile2_001",               # Auto: Edited
+  "eliza_ex_006_smile_001":                "eliza_ex_006_smile_001",                # Auto: Edited
+  "eliza_ex_007_003":                      "eliza_ex_013_003",
+  "eliza_ex_007_open_003":                 "eliza_ex_013_open_003",
+  "eliza_ex_008_003":                      "eliza_ex_008_003",                      # Auto: Edited
+  "eliza_ex_008_open_003":                 "eliza_ex_008_open_003",                 # Auto: Edited
+  "eliza_ex_009_002":                      "eliza_ex_009_open_002",
+  "eliza_ex_009_closed_002":               "eliza_ex_009_closed_002",               # Auto: Edited
+  "eliza_ex_009_open_003":                 "eliza_ex_009_open_002",
+  "eliza_ex_010_002":                      "eliza_ex_010_open_002",
+  "eliza_ex_011_003":                      "eliza_ex_011_003",                      # Auto: Edited
+  "eliza_ex_012_002":                      "eliza_ex_012_002",                      # Auto: Edited
+  "eliza_ex_013_002":                      "eliza_ex_012_smile_002",
+  "eliza_ex_014_002":                      "eliza_ex_010_002",
+  "eliza_ex_014_daze2_002":                "eliza_ex_010_daze2_002",
+  "eliza_ex_014_daze_002":                 "eliza_ex_010_daze_002",
+  "eliza_ex_015_002":                      "eliza_ex_011_smile_002",
+  "eliza_ex_016_003":                      "eliza_ex_013_smile_003",
+  "eliza_ex_full_001_001":                 "eliza_ex_full_001_001",                 # Auto: Same
+  "eliza_ex_full_001_empty_001":           "eliza_ex_full_001_empty_001",           # Auto: Same
+  "eliza_ex_full_001_neutral_001":         "eliza_ex_full_001_neutral_001",         # Auto: Same
+  "eliza_ex_full_001_open_001":            "eliza_ex_full_001_open_001",            # Auto: Same
+  "eliza_ex_full_001_smile2_001":          "eliza_ex_full_001_smile2_001",          # Auto: Same
+  "eliza_ex_full_001_smile_001":           "eliza_ex_full_001_smile_001",           # Auto: Same
+  "eliza_ex_full_002_001":                 "eliza_ex_full_002_001",                 # Auto: Same
+  "eliza_ex_full_002_open_001":            "eliza_ex_full_002_open_001",            # Auto: Same
+  "eliza_ex_full_002_smile_001":           "eliza_ex_full_002_smile_001",           # Auto: Same
+  "eliza_ex_full_003_001":                 "eliza_ex_full_003_001",                 # Auto: Same
+  "eliza_ex_full_003_grin2_001":           "eliza_ex_full_003_grin2_001",           # Auto: Same
+  "eliza_ex_full_003_grin_001":            "eliza_ex_full_003_grin_001",            # Auto: Same
+  "eliza_ex_full_003_grit_001":            "eliza_ex_full_003_grit_001",            # Auto: Same
+  "eliza_ex_full_003_irked2_003":          "eliza_ex_full_003_irked4_003",          # Auto: Renamed
+  "eliza_ex_full_003_irked_003":           "eliza_ex_full_003_irked3_003",          # Auto: Renamed
+  "eliza_ex_full_003_open2_001":           "eliza_ex_full_003_open2_001",           # Auto: Same
+  "eliza_ex_full_003_open_001":            "eliza_ex_full_003_open_001",            # Auto: Same
+  "eliza_ex_full_004_001":                 "eliza_ex_full_004_001",                 # Auto: Same
+  "eliza_ex_full_004_open2_001":           "eliza_ex_full_004_open2_001",           # Auto: Same
+  "eliza_ex_full_004_open_001":            "eliza_ex_full_004_open_001",            # Auto: Same
+  "eliza_ex_full_004_smile_001":           "eliza_ex_full_004_smile_001",           # Auto: Same
+  "eliza_ex_full_005_001":                 "eliza_ex_full_005_001",                 # Auto: Same
+  "eliza_ex_full_005_aroused2_001":        "eliza_ex_full_005_aroused2_001",        # Auto: Same
+  "eliza_ex_full_005_aroused_001":         "eliza_ex_full_005_aroused_001",         # Auto: Same
+  "eliza_ex_full_005_open2_001":           "eliza_ex_full_005_open2_001",           # Auto: Same
+  "eliza_ex_full_005_open_001":            "eliza_ex_full_005_open_001",            # Auto: Same
+  "eliza_ex_full_005_smile2_001":          "eliza_ex_full_005_smile2_001",          # Auto: Same
+  "eliza_ex_full_005_smile_001":           "eliza_ex_full_005_smile_001",           # Auto: Same
+  "eliza_ex_full_006_001":                 "eliza_ex_full_006_001",                 # Auto: Same
+  "eliza_ex_full_006_open2_001":           "eliza_ex_full_006_open2_001",           # Auto: Same
+  "eliza_ex_full_006_open_001":            "eliza_ex_full_006_open_001",            # Auto: Same
+  "eliza_ex_full_006_smile2_001":          "eliza_ex_full_006_smile2_001",          # Auto: Same
+  "eliza_ex_full_006_smile_001":           "eliza_ex_full_006_smile_001",           # Auto: Same
+  "eliza_ex_full_007_003":                 "eliza_ex_full_013_003",                 # Auto: Renamed
+  "eliza_ex_full_007_open_003":            "eliza_ex_full_013_open_003",            # Auto: Renamed
+  "eliza_ex_full_008_003":                 "eliza_ex_full_008_003",                 # Auto: Same
+  "eliza_ex_full_008_open_003":            "eliza_ex_full_008_open_003",            # Auto: Same
+  "eliza_ex_full_009_002":                 "eliza_ex_full_009_open_002",
+  "eliza_ex_full_009_closed_002":          "eliza_ex_full_009_closed2_002",         # Auto: Renamed
+  "eliza_ex_full_009_open_003":            "eliza_ex_full_009_open2_003",           # Auto: Renamed
+  "eliza_ex_full_010_002":                 "eliza_ex_full_010_open_002",            # Auto: Renamed
+  "eliza_ex_full_011_003":                 "eliza_ex_full_011_003",                 # Auto: Same
+  "eliza_ex_full_012_002":                 "eliza_ex_full_012_002",                 # Auto: Same
+  "eliza_ex_full_013_002":                 "eliza_ex_full_012_smile_002",           # Auto: Renamed
+  "eliza_ex_full_014_002":                 "eliza_ex_full_010_002",                 # Auto: Renamed
+  "eliza_ex_full_014_daze2_002":           "eliza_ex_full_010_daze2_002",           # Auto: Renamed
+  "eliza_ex_full_014_daze_002":            "eliza_ex_full_010_daze_002",            # Auto: Renamed
+  "eliza_ex_full_015_002":                 "eliza_ex_full_011_smile_002",           # Auto: Renamed
+  "eliza_ex_full_016_003":                 "eliza_ex_full_013_smile_003",           # Auto: Renamed
 }
-
--- compare_character.py
+++ compare_character.py
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------------------
 # compare_character.py
 # Checks an extracted character's graphics to see what's changed between
-# version 0.3 and 0.5
+# versions
 #-----------------------------------------------------------------------------
 
 import getopt
@@ -16,7 +16,7 @@ import zipfile
 filename_03 = "Press-SwitchV0.3b-all";
 filename_04 = "Press-SwitchV0.4a-pc";
 filename_05 = "Press-SwitchV0.5c-pc";
-
+filename_06 = "Press-SwitchV0.6";
 
 #-----------------------------------------------------------------------------
 def printGreen(s):
@@ -102,70 +102,67 @@ def main(argv):
     asArray = True;
 
   characterName = argv[0];
-  v3char = characterName;
-  if (characterName == "Chris"):
-    v3char = "Christine";
-  srcDir3 = os.path.join("Extracted", filename_03, "Character images", v3char);
   srcDir5 = os.path.join("Extracted", filename_05, "Characters", characterName);
+  srcDir6 = os.path.join("Extracted", filename_06, "Characters", characterName);
 
   # Sanity check
-  if (not(os.path.isdir(pathlib.Path(srcDir3)))):
-    showError("Character " + characterName + " not found in 0.3 Graphics directory");
-    sys.exit(1);
   if (not(os.path.isdir(pathlib.Path(srcDir5)))):
     showError("Character " + characterName + " not found in 0.5 Graphics directory");
     sys.exit(1);
+  if (not(os.path.isdir(pathlib.Path(srcDir6)))):
+    showError("Character " + characterName + " not found in 0.6 Graphics directory");
+    sys.exit(1);
 
-  srcDir3Crop = srcDir3 + "Cropped";
   srcDir5Crop = srcDir5 + "Cropped";
-
-  # Do we need to crop images for 0.3?
-  if (not(os.path.isdir(pathlib.Path(srcDir3Crop)))):
-    doCrop(srcDir3);
+  srcDir6Crop = srcDir6 + "Cropped";
 
   # Do we need to crop images for 0.5?
   if (not(os.path.isdir(pathlib.Path(srcDir5Crop)))):
     doCrop(srcDir5);
 
-  # Build an index of the MD5 hashes for 0.5
-  idx = buildIndex(srcDir5Crop);
+  # Do we need to crop images for 0.6?
+  if (not(os.path.isdir(pathlib.Path(srcDir6Crop)))):
+    doCrop(srcDir6);
 
-  # Sort the list of files in 0.3
-  fileList = os.listdir(srcDir3Crop);
+  # Build an index of the MD5 hashes for 0.6
+  idx = buildIndex(srcDir6Crop);
+
+  # Sort the list of files in 0.5
+  fileList = os.listdir(srcDir5Crop);
   fileList.sort();
 
   if asArray:
     print("characterMap" + characterName + " = {");
 
-  # Iterate 0.3
+  # Iterate 0.5
   fileWidth = 40;
   for pic in fileList:
     if pic.endswith(".pnm"):
-      chksum = md5(os.path.join(srcDir3Crop, pic));
+      chksum = md5(os.path.join(srcDir5Crop, pic));
       file1 = insertNumber(pic, "_001");
       file2 = insertNumber(pic, "_002");
       file3 = insertNumber(pic, "_003");
 
       if asArray:
         if (chksum in idx):
-          filename5 = idx.get(chksum);
-          if filename5 == pic:
+          filename6 = idx.get(chksum);
+          if filename6 == pic:
             # Same
             printArrayEntry(pic, pic, "Same");
           else:
             # Renamed
-            printArrayEntry(pic, filename5, "Renamed");
+            printArrayEntry(pic, filename6, "Renamed");
         else:
-          if os.path.exists(os.path.join(srcDir5Crop, pic)):
+          if os.path.exists(os.path.join(srcDir6Crop, pic)):
             # Edited
             printArrayEntry(pic, pic, "Edited");
-          elif os.path.exists(os.path.join(srcDir5Crop, file1)):
+          elif os.path.exists(os.path.join(srcDir6Crop, file1)):
             # Edited and renamed
             printArrayEntry(pic, file1, "Edited and renamed");
-          elif os.path.exists(os.path.join(srcDir5Crop, file2)):
+          elif os.path.exists(os.path.join(srcDir6Crop, file2)):
             # Edited and renamed
             printArrayEntry(pic, file2, "Edited and renamed");
-          elif os.path.exists(os.path.join(srcDir5Crop, file3)):
+          elif os.path.exists(os.path.join(srcDir6Crop, file3)):
             # Edited and renamed
             printArrayEntry(pic, file3, "Edited and renamed");
           else:
@@ -173,13 +170,13 @@ def main(argv):
             printArrayEntry(pic, "", "Deleted");
       else:
         if chksum in idx:
-          filename5 = idx.get(chksum);
-          if filename5 == pic:
+          filename6 = idx.get(chksum);
+          if filename6 == pic:
             print(leftAlign(pic, fileWidth) + ": Same");
           else:
-            printGreen(leftAlign(pic, fileWidth) + ": Renamed to " + filename5);
+            printGreen(leftAlign(pic, fileWidth) + ": Renamed to " + filename6);
         else:
-          if os.path.exists(os.path.join(srcDir5Crop, pic)):
+          if os.path.exists(os.path.join(srcDir6Crop, pic)):
             printYellow(leftAlign(pic, fileWidth) + ": Edited");
           else:
             printRed(leftAlign(pic, fileWidth) + ": Deleted");
