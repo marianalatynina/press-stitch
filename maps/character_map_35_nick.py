5f5175713d75bea74e16d489194346ee6f0b9e37
Mariana <mariana.latynina@gmail.com>
2022-03-06 17:41:18 +0000

    Update Nick expression mapping for 0.3 -> 0.5

--- maps/character_map_35_nick.py
+++ maps/character_map_35_nick.py
@@ -1,12 +1,11 @@
 characterMapNick = {
-    "nick_base_001":                         "nick_base_001",                         # Auto: Edited
-    "nick_ex_001":                           "nick_ex_001_001",                       # Auto: Edited and renamed
-    "nick_ex_002":                           "nick_ex_002_001",                       # Auto: Edited and renamed
-    "nick_ex_003":                           "nick_ex_003_001",                       # Auto: Edited and renamed
-    "nick_ex_004":                           "nick_ex_004_001",                       # Auto: Edited and renamed
-    "nick_ex_005":                           "nick_ex_005_001",                       # Auto: Edited and renamed
-    "nick_ex_006":                           "nick_ex_006_001",                       # Auto: Edited and renamed
-    "nick_ex_007":                           "nick_ex_007_001",                       # Auto: Edited and renamed
-    "nick_ex_008":                           "nick_ex_008_001",                       # Auto: Edited and renamed
-    "nick_ex_009":                           "nick_ex_009_001",                       # Auto: Edited and renamed
+    "nick_ex_001":                           "nick_ex_003_grin_001",
+    "nick_ex_002":                           "nick_ex_003_irked2_001",
+    "nick_ex_003":                           "nick_ex_001_001",
+    "nick_ex_004":                           "nick_ex_003_grit_001",
+    "nick_ex_005":                           "nick_ex_005_smile_001",
+    "nick_ex_006":                           "nick_ex_005_001",
+    "nick_ex_007":                           "nick_ex_008_001",  # not exact
+    "nick_ex_008":                           "nick_ex_008_001",
+    "nick_ex_009":                           "nick_ex_001_smile_001",
 }
